f58d8132c206d5a7510468a495dc235cc71e38a9
Tales Macedo <talesmgbahia@hotmail.com>
2026-02-05 16:49:33 -0300

    feat(cliente_routes.py): adicionando rota de consulta de cliente por cpf ou telefone

--- backend/app/routes/cliente_routes.py
+++ backend/app/routes/cliente_routes.py
@@ -1,10 +1,29 @@
 from flask import Blueprint, request, jsonify
 from app.services.cliente_service import consultar_cliente
 
-cliente_bp = Blueprint("cliente", __name__, url_prefix="/cliente")
+cliente_bp = Blueprint("cliente", __name__, url_prefix="/clientes")
+
 
 @cliente_bp.route("/consultar", methods=["GET"])
 def consultar():
-    #dados = request.json
-    resultado = consultar_cliente("dados")
-    return {"status": "Consultado"} #jsonify(resultado)
+    cpf = request.args.get("cpf")
+    telefone = request.args.get("telefone")
+
+    if not cpf and not telefone:
+        return jsonify({
+            "error": "Informe cpf ou telefone"
+        }), 400
+
+    cliente = consultar_cliente(cpf=cpf, telefone=telefone)
+    print(cliente)
+    if not cliente:
+        return jsonify({
+            "found": False,
+            "message": "Cliente não encontrado"
+        }), 404
+
+    return jsonify({
+        "found": True,
+        "cliente": cliente
+    })
+
