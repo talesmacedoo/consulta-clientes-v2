0f536ef2c6d2a76b0a6f24cde9820d45650126fa
Tales Macedo <talesmgbahia@hotmail.com>
2026-02-05 16:46:04 -0300

    feat(app.py): adicioando safe=true na configuracao do banco de dados

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,14 @@
 from flask import Flask
 from config import Config
 from app.routes import register_routes
+from app.database import db
+from app.models.cliente import Cliente
+
+db.connect()
+db.create_tables([Cliente], safe=True)
+
+
+
 
 def create_app():
     app = Flask(__name__)
