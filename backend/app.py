7c3c0f39275af5130290c1bfe447c47a9b94dd36
Tales Macedo <talesmgbahia@hotmail.com>
2026-02-09 15:36:11 -0300

    feat(app): adicionando CORS

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from config import Config
 from app.routes import register_routes
 from app.database import db
@@ -12,6 +13,7 @@ db.create_tables([Cliente], safe=True)
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(Config)
 
     register_routes(app)
