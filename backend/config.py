c5b3f7f1a287f8df7b93a616fceac335265b87cf
Tales Macedo <talesmgbahia@hotmail.com>
2026-04-20 16:17:48 -0300

    chore(backend): adiciona suporte a variáveis de ambiente e bibliotecas http

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config():
     DEBUG = True
     TESTING = False
     SQLALCHEMY_DATABASE_URI = "sqlite:///database/local_database.db"
+    API_BASE_URL = os.environ.get("API_BASE_URL", "http://192.168.1.60:5001")
